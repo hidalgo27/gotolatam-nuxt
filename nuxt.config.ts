ea3b8b263046cf513837a600825c998c7b167f6c
Hidalgo Edely Chata Ponce <hidalgochponce@gmail.com>
2023-06-14 18:45:40 -0500

    Base de la aplicacion

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,15 @@ export default defineNuxtConfig({
     '~/assets/css/main.css',
     `~/assets/css/page.css`
   ],
+  apps: [
+    {
+      name: 'NuxtAppName',
+      port: '3000',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './.output/server/index.mjs'
+    }
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
