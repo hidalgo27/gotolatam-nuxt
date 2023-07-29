601d8d926d501b65ac78f2c9aa3861479a131805
Hidalgo Edely Chata Ponce <hidalgochponce@gmail.com>
2023-07-29 13:19:32 -0500

    Version 1 home

---
+++ components/CarouselP.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="container">
+    <div class="grid grid-cols-7">
+      <div class="col-span-2">
+        <h2 class="text-3xl font-bold">Top <br>12 tours</h2>
+        <p class="my-6">We specialize inn <br> brinding you the <br> finest n luxury <br> South American <br>tours.</p>
+        <button type="button" class="btn-ternary-outline flex gap-2 my-6 items-center">
+          <img src="/icons/map.svg" alt="">
+          View all
+        </button>
+        <div class="flex gap-6">
+          <button class="rounded-full border-2 p-2  border-gray-500 focus:bg-gray-800 focus:text-white" @click="prevSlide">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6 mx-auto">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+            </svg>
+          </button>
+          <button class="rounded-full border-2 p-2  border-gray-500 focus:bg-gray-800 focus:text-white" @click="nextSlide">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      <div class="col-span-5">
+        <Carousel  ref="carousel" :wrap-around="true" :breakpoints="breakpoints">
+          <slide v-for="item in items" :key="item.id">
+            <!-- Aquí puedes poner el contenido de cada slide, por ejemplo: -->
+            <div class="mx-3 p-3 bg-white w-full rounded-lg my-2 shadow-md">
+              <div class="relative">
+                <img src="/images/packages/package1.png" alt="">
+                <div class="bg-secondary px-2 py-1 rounded w-auto absolute bottom-0 -mb-2 m-2 text-[9px] font-semibold text-white">PAQUETE</div>
+              </div>
+              <div class="">
+                <h3 class="text-left text-lg font-semibold my-3">{{ item.title }}</h3>
+                <div class="flex text-xs font-semibold gap-1 items-center">
+                  PERU
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5 text-orange-400">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                  </svg>
+                  COLOMBIA
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5 text-orange-400">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                  </svg>
+                  BRASIL
+                </div>
+                <div class="flex gap-2 mt-3 text-sm">
+                  <img src="/icons/map-location.svg" alt=""> Estaring Airpor <span class="text-primary">Cuzco</span>
+                </div>
+                <div class="border my-4"></div>
+                <div class="flex justify-between text-lg font-semibold">
+                  <div>15 days</div>
+                  <div><span class="text-xs text-gray-400">From</span> $350</div>
+                </div>
+              </div>
+            </div>
+          </slide>
+        </Carousel>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide } from 'vue3-carousel';
+export default {
+  name: "CarouselP",
+  components: {
+    Carousel,
+    Slide,
+  },
+  data() {
+    return {
+      items: [
+        { id: 1, title: 'Slide 1', description: 'Description for slide 1' },
+        { id: 2, title: 'Slide 2', description: 'Description for slide 2' },
+        { id: 3, title: 'Slide 3', description: 'Description for slide 3' },
+      ],
+      breakpoints: {
+        // 700px and up
+        700: {
+          itemsToShow: 3.5,
+          snapAlign: 'center',
+        },
+        // 1024 and up
+        1024: {
+          itemsToShow: 3.2,
+          snapAlign: 'start',
+        },
+      },
+    };
+  },
+  methods: {
+    prevSlide() {
+      this.$refs.carousel.prev();
+    },
+    nextSlide() {
+      this.$refs.carousel.next();
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -69,21 +69,219 @@
 
     </div>
 
-    <carousel :items-to-show="1.5">
-      <slide v-for="slide in 10" :key="slide">
-        {{ slide }}
-      </slide>
+    <section class="py-12 bg-gray-100">
+      <carousel-p></carousel-p>
+    </section>
 
-      <template #addons>
-        <navigation />
-        <pagination />
-      </template>
-    </carousel>
-<!--    <div class="container">-->
-<!--      <div class="">-->
-<!--        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ex facilis fuga illo inventore labore natus reiciendis tempore unde. Cumque deserunt, fugiat ipsa libero magnam perferendis quisquam! At, doloremque, eum?-->
-<!--      </div>-->
-<!--    </div>-->
+    <section class="my-12 container">
+      <div class="w-10/12 mx-auto">
+        <div class="grid grid-cols-2 justify-center gap-24">
+          <div class="">
+            <div class="columns-2 gap-3 space-y-4 floa">
+              <img src="/images/home/operator1.png" alt="">
+              <img src="/images/home/operator2.png" alt="">
+              <img src="/images/home/operator3.png" alt="">
+              <img src="/images/home/operator4.png" alt="">
+            </div>
+          </div>
+
+          <div>
+            <h2 class="text-3xl font-bold">LATAM Travel Operator</h2>
+            <p class="my-6">Trust our team of local travel experts to <br> plan an unforgettable trip full of <br> must-see attractions and our favorite
+              <br> hidden gems of the land <br> of the Incas. Every Peru tour offered is <br>regularly inspected to ensure our travelers
+              <br> have an amazing experience.</p>
+            <button type="button" class="btn-primary">Create My Trip Now</button>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="py-12 bg-gray-100">
+      <carousel-h></carousel-h>
+    </section>
+
+    <section class="container py-12">
+      <h2 class="text-3xl font-bold text-center">Destinations</h2>
+      <div class="flex justify-center gap-3 my-6">
+        <button type="button" class="px-5 py-2 bg-[#D6DD85] text-primary font-medium rounded-full">Perú</button>
+        <button type="button" class="px-5 py-2 bg-gray-100 text-gray-800 font-medium rounded-full">Bolivia</button>
+        <button type="button" class="px-5 py-2 bg-gray-100 text-gray-800 font-medium rounded-full">Chile</button>
+        <button type="button" class="px-5 py-2 bg-gray-100 text-gray-800 font-medium rounded-full">Ecuador</button>
+        <button type="button" class="px-5 py-2 bg-gray-100 text-gray-800 font-medium rounded-full">Colombia</button>
+        <button type="button" class="px-5 py-2 bg-gray-100 text-gray-800 font-medium rounded-full">Argentina</button>
+        <button type="button" class="px-5 py-2 bg-gray-100 text-gray-800 font-medium rounded-full">Brasil</button>
+      </div>
+      <div class="grid grid-cols-5 gap-24 my-12">
+        <div class="col-span-2">
+          <h3 class="text-3xl font-bold">simply dummy text of</h3>
+          <div class="pr-24">
+            <p class="my-6">is simply dummy text of the printing an pesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown is simply dummy text of the printing an pesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever</p>
+          </div>
+        </div>
+        <div class="col-span-3">
+          <img src="https://gotoperu.com/images/1327.jpg" alt="">
+        </div>
+      </div>
+    </section>
+
+    <section class="py-12 bg-gray-100">
+      <div class="container">
+        <h2 class="text-3xl font-bold text-center">Reviews and Testimonials</h2>
+        <div class="columns-4 gap-6 space-y-6 my-12">
+          <div class="relative rounded-2xl">
+              <div class="relative">
+                <img src="/images/testimonials/testimonial1.png" alt="" class="w-full object-center">
+                <div class="absolute inset-x-0 top-0 h-full pb-12 flex items-center">
+                  <img src="/icons/pay.svg" alt="" class="mx-auto">
+                </div>
+              </div>
+            <div class="absolute inset-x-0 bottom-0 bg-gray-800 rounded-b-2xl px-6 pb-3 pt-2 text-white">
+              <img src="/icons/quotes.svg" alt="" class="-mt-9">
+              <h3 class="text-sm my-2">Zeeshan B. / Toronto - Canada</h3>
+              <p class="text-xs">…I would recommend GotoLatam 100%. You will not regret it!!!</p>
+            </div>
+          </div>
+          <div class="relative rounded-2xl">
+              <div class="relative">
+                <img src="/images/testimonials/testimonial2.png" alt="" class="w-full object-center">
+                <div class="absolute inset-x-0 top-0 h-full pb-12 flex items-center">
+                  <img src="/icons/pay.svg" alt="" class="mx-auto">
+                </div>
+              </div>
+            <div class="absolute inset-x-0 bottom-0 bg-gray-800 rounded-b-2xl px-6 pb-3 pt-2 text-white">
+              <img src="/icons/quotes.svg" alt="" class="-mt-9">
+              <h3 class="text-sm my-2">Zeeshan B. / Toronto - Canada</h3>
+              <p class="text-xs">…I would recommend GotoLatam 100%. You will not regret it!!!</p>
+            </div>
+          </div>
+          <div class="relative rounded-2xl">
+              <div class="relative">
+                <img src="/images/testimonials/testimonial3.png" alt="" class="w-full object-center">
+                <div class="absolute inset-x-0 top-0 h-full pb-12 flex items-center">
+                  <img src="/icons/pay.svg" alt="" class="mx-auto">
+                </div>
+              </div>
+            <div class="absolute inset-x-0 bottom-0 bg-gray-800 rounded-b-2xl px-6 pb-3 pt-2 text-white">
+              <img src="/icons/quotes.svg" alt="" class="-mt-9">
+              <h3 class="text-sm my-2">Zeeshan B. / Toronto - Canada</h3>
+              <p class="text-xs">…I would recommend GotoLatam 100%. You will not regret it!!!</p>
+            </div>
+          </div>
+          <div class="relative rounded-2xl">
+              <div class="relative">
+                <img src="/images/testimonials/testimonial4.png" alt="" class="w-full object-center">
+                <div class="absolute inset-x-0 top-0 h-full pb-12 flex items-center">
+                  <img src="/icons/pay.svg" alt="" class="mx-auto">
+                </div>
+              </div>
+            <div class="absolute inset-x-0 bottom-0 bg-gray-800 rounded-b-2xl px-6 pb-3 pt-2 text-white">
+              <img src="/icons/quotes.svg" alt="" class="-mt-9">
+              <h3 class="text-sm my-2">Zeeshan B. / Toronto - Canada</h3>
+              <p class="text-xs">…I would recommend GotoLatam 100%. You will not regret it!!!</p>
+            </div>
+          </div>
+          <div class="relative rounded-2xl">
+              <div class="relative">
+                <img src="/images/testimonials/testimonial2.png" alt="" class="w-full object-center">
+                <div class="absolute inset-x-0 top-0 h-full pb-12 flex items-center">
+                  <img src="/icons/pay.svg" alt="" class="mx-auto">
+                </div>
+              </div>
+            <div class="absolute inset-x-0 bottom-0 bg-gray-800 rounded-b-2xl px-6 pb-3 pt-2 text-white">
+              <img src="/icons/quotes.svg" alt="" class="-mt-9">
+              <h3 class="text-sm my-2">Zeeshan B. / Toronto - Canada</h3>
+              <p class="text-xs">…I would recommend GotoLatam 100%. You will not regret it!!!</p>
+            </div>
+          </div>
+          <div class="relative rounded-2xl">
+              <div class="relative">
+                <img src="/images/testimonials/testimonial5.png" alt="" class="w-full object-center">
+                <div class="absolute inset-x-0 top-0 h-full pb-12 flex items-center">
+                  <img src="/icons/pay.svg" alt="" class="mx-auto">
+                </div>
+              </div>
+            <div class="absolute inset-x-0 bottom-0 bg-gray-800 rounded-b-2xl px-6 pb-3 pt-2 text-white">
+              <img src="/icons/quotes.svg" alt="" class="-mt-9">
+              <h3 class="text-sm my-2">Zeeshan B. / Toronto - Canada</h3>
+              <p class="text-xs">…I would recommend GotoLatam 100%. You will not regret it!!!</p>
+            </div>
+          </div>
+          <div class="relative rounded-2xl">
+              <div class="relative">
+                <img src="/images/testimonials/testimonial3.png" alt="" class="w-full object-center">
+                <div class="absolute inset-x-0 top-0 h-full pb-12 flex items-center">
+                  <img src="/icons/pay.svg" alt="" class="mx-auto">
+                </div>
+              </div>
+            <div class="absolute inset-x-0 bottom-0 bg-gray-800 rounded-b-2xl px-6 pb-3 pt-2 text-white">
+              <img src="/icons/quotes.svg" alt="" class="-mt-9">
+              <h3 class="text-sm my-2">Zeeshan B. / Toronto - Canada</h3>
+              <p class="text-xs">…I would recommend GotoLatam 100%. You will not regret it!!!</p>
+            </div>
+          </div>
+        </div>
+        <div class="flex gap-2 items-center justify-center">
+          <p>Happy Guests are the Best Rewarc</p>
+          <img src="/images/logos/tripadvisor.png" alt="" class="w-32">
+          <img src="/images/logos/trustpilot.png" alt="" class="w-20">
+        </div>
+      </div>
+    </section>
+
+    <section class="container py-16">
+      <div class="grid grid-cols-4 gap-6">
+        <div class="grid grid-cols-2 rounded-xl relative bg-gray-100 px-4 py-8 border gap-12">
+          <img src="/icons/shield.svg" alt="" class="absolute left-0 top-0 ml-3 -mt-6">
+          <div class="">
+            <h3 class="font-semibold text-sm">Book with Confidence</h3>
+            <p class="text-sm mt-3">Keep calm! We will help you plan your dream trip.</p>
+          </div>
+          <div class="">
+            <div class="bg-primary rounded-xl h-full w-full"></div>
+          </div>
+        </div>
+        <div class="grid grid-cols-2 rounded-xl relative bg-gray-100 px-4 py-8 border gap-12">
+          <img src="/icons/star-yellow.svg" alt="" class="absolute left-0 top-0 ml-3 -mt-6">
+          <div class="">
+            <h3 class="font-semibold text-sm">Book with Confidence</h3>
+            <p class="text-sm mt-3">Keep calm! We will help you plan your dream trip.</p>
+          </div>
+          <div class="">
+            <div class="bg-primary rounded-xl h-full w-full"></div>
+          </div>
+        </div>
+        <div class="grid grid-cols-2 rounded-xl relative bg-gray-100 px-4 py-8 border gap-12">
+          <img src="/icons/person.svg" alt="" class="absolute left-0 top-0 ml-3 -mt-6">
+          <div class="">
+            <h3 class="font-semibold text-sm">Book with Confidence</h3>
+            <p class="text-sm mt-3">Keep calm! We will help you plan your dream trip.</p>
+          </div>
+          <div class="">
+            <div class="bg-primary rounded-xl h-full w-full"></div>
+          </div>
+        </div>
+        <div class="grid grid-cols-2 rounded-xl relative bg-gray-100 px-4 py-8 border gap-12">
+          <img src="/icons/shield.svg" alt="" class="absolute left-0 top-0 ml-3 -mt-6">
+          <div class="">
+            <h3 class="font-semibold text-sm">Book with Confidence</h3>
+            <p class="text-sm mt-3">Keep calm! We will help you plan your dream trip.</p>
+          </div>
+          <div class="">
+            <div class="bg-primary rounded-xl h-full w-full"></div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="container pb-16 text-center">
+      <img src="/images/logos/logo-gotolatam.svg" alt="" class="mx-auto w-52">
+      <p class="my-6">We specialize in bringing you the finest in luxury South American Tours</p>
+    </section>
+
+    <footer class="h-72 bg-gray-800">
+
+    </footer>
+
 
     <section class="modal-box">
       <transition name="fade" appear>
@@ -114,9 +312,8 @@
 
 
 <script lang="ts" setup>
-import 'vue3-carousel/dist/carousel.css'
-import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 
+const currentSlide = ref(0)
 const showModalProcess = ref(false)
 const dateValue = ref([])
 const destination = ref()
@@ -126,6 +323,7 @@ const formatter = ref({
   date: 'YYYY/MM/DD',
   month: 'MMM'
 })
+
 
   function openPopover(val:number){
     if (val){
